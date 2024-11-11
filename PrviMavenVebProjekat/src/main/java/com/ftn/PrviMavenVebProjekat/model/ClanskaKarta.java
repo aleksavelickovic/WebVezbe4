8e4901fb23d9fbdfff01e11b11ace06a293bbf93
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-11-11 23:57:10 +0100

    Dodata ClanskaKarta.java

--- PrviMavenVebProjekat/src/main/java/com/ftn/PrviMavenVebProjekat/model/ClanskaKarta.java
+++ PrviMavenVebProjekat/src/main/java/com/ftn/PrviMavenVebProjekat/model/ClanskaKarta.java
@@ -3,11 +3,12 @@ package com.ftn.PrviMavenVebProjekat.model;
 import java.util.List;
 
 public class ClanskaKarta {
-	
+
 	private String registarskiBroj;
 	private List<Knjiga> iznajmljenjeKnjige;
-	
-	public ClanskaKarta() {}
+
+	public ClanskaKarta() {
+	}
 
 	public ClanskaKarta(String registarskiBroj, List<Knjiga> iznajmljenjeKnjige) {
 		super();
@@ -15,11 +16,19 @@ public class ClanskaKarta {
 		this.iznajmljenjeKnjige = iznajmljenjeKnjige;
 	}
 
-	public String getRegistarskiBroj() { return registarskiBroj; }
+	public String getRegistarskiBroj() {
+		return registarskiBroj;
+	}
 
-	public void setRegistarskiBroj(String registarskiBroj) { this.registarskiBroj = registarskiBroj; }
+	public void setRegistarskiBroj(String registarskiBroj) {
+		this.registarskiBroj = registarskiBroj;
+	}
 
-	public List<Knjiga> getIznajmljenjeKnjige() { return iznajmljenjeKnjige; }
+	public List<Knjiga> getIznajmljenjeKnjige() {
+		return iznajmljenjeKnjige;
+	}
 
-	public void setIznajmljenjeKnjige(List<Knjiga> iznajmljenjeKnjige) { this.iznajmljenjeKnjige = iznajmljenjeKnjige; }
+	public void setIznajmljenjeKnjige(List<Knjiga> iznajmljenjeKnjige) {
+		this.iznajmljenjeKnjige = iznajmljenjeKnjige;
+	}
 }
